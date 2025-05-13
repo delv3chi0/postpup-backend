8b95f18e3df5fcd7dc7b5518dcafc97a2f47764b
delv3chi0 <delv3chio@gmail.com>
2025-05-13 07:53:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the PostPup API');
+});
+
 app.get('/test', (req, res) => {
   res.json({ message: 'PostPup backend is working!' });
 });
